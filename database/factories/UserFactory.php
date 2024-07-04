258f74b90de3d4c6f8fe98b59068122b167fc192
Josef Adlao <jadlao@gmail.com>
2024-07-04 15:59:17 +1000

    added company test case

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -5,6 +5,7 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
+use App\Enums\Role;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
@@ -29,6 +30,7 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
+            'role_id' => Role::CUSTOMER->value, 
         ];
     }
 
@@ -41,4 +43,11 @@ class UserFactory extends Factory
             'email_verified_at' => null,
         ]);
     }
+
+    public function admin(): static 
+    {
+        return $this->state(fn (array $attributes) => [
+            'role_id' => Role::ADMINISTRATOR->value,
+        ]);
+    } 
 }
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,12 @@
 
 namespace Tests;
 
+use Database\Seeders\RoleSeeder;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    // use CreatesApplication;
+ 
+    protected bool $seed = true; 
 }
