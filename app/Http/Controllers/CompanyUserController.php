b2668e93cfdae74a224503ca91f1a2229e0ee99f
Josef Adlao <jadlao@gmail.com>
2024-07-21 21:43:44 +1000

    fixing mail registration tests

--- app/Http/Controllers/CompanyUserController.php
+++ app/Http/Controllers/CompanyUserController.php
@@ -34,25 +34,35 @@ class CompanyUserController extends Controller
  
     public function store(StoreUserRequest $request, Company $company)
     {
-        Gate::authorize('create', $company); 
+        Gate::authorize('create', $company);
 
-        // $company->users()->create([
-        //     'name' => $request->input('name'),
-        //     'email' => $request->input('email'),
-        //     'password' => bcrypt($request->input('password')),
-        //     'role_id' => Role::COMPANY_OWNER->value,
-        // ]);
+        $email = $request->input('email');
 
-        $invitation = UserInvitation::create([
-            'email' => $request->input('email'),
-            'token' => Str::uuid(),
-            'company_id' => $company->id,
-            'role_id' => Role::COMPANY_OWNER->value,
-        ]);
- 
-        Mail::to($request->input('email'))->send(new RegistrationInvite($invitation));
- 
-        return to_route('companies.users.index', $company);
+        // Check if the email already exists in the user_invitations table
+        $existingInvitation = UserInvitation::where('email', $email)->first();
+
+        if ($existingInvitation) {
+            // If the email already exists, set an error in the session
+            session()->flash('errors', ['email' => 'Invitation with this email address already requested.']);
+
+            // 2. Return an error response
+            // return response()->json(['error' => 'Email already has a pending invitation'], 400);
+            // return to_route('companies.users.index', $company);
+            // Use the session()->get('errors') method to ensure a MessageBag instance
+            return redirect()->back()->withErrors(session()->get('errors'));
+        } else {
+            // Create a new invitation
+            $invitation = UserInvitation::create([
+                'email' => $email,
+                'token' => Str::uuid(),
+                'company_id' => $company->id,
+                'role_id' => Role::COMPANY_OWNER->value,
+            ]);
+
+            Mail::to($email)->send(new RegistrationInvite($invitation));
+
+            return to_route('companies.users.index', $company);
+        }
     }
  
     public function edit(Company $company, User $user)
